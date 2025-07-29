cef834bbf42b07a9b7c3ba16758af4518470dc2a
4simb <mookhachev.denis@gmail.com>
2025-07-29 13:28:03 +0300

    Add class Unit

---
+++ lib/Unit.cpp
@@ -0,0 +1,3 @@
+// Robocamp 2025 - rock, paper, scissors
+#include "Unit.h"
+
--- lib/Unit.h
+++ lib/Unit.h
@@ -1,1 +1,24 @@
 // Robocamp 2025 - rock, paper, scissors
+#pragma once
+
+#include <string>
+
+enum Choice : int { ROCK, PAPER, SCISSORS };
+
+class Unit {
+   public:
+    virtual Choice MakeChoice() = delete;
+
+    virtual void GetResult(Choice enemy_choice) = delete;
+
+    virtual void StartBattle(int win_count) = delete;
+
+   private:
+    int win_count_;
+
+    const std::string name_;
+
+    const std::string win_phrase_;
+
+    const std::string lose_phrase;
+};
