80e4411544d8faa413f72eaff703c6632cb746d9
T1000trap-1 <ttrap1012@gmail.com>
2025-07-30 11:17:51 +0300

    Stalin

--- Unit_Lenin.cpp
+++ Unit_Lenin.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 #include <Unit_Lenin.h>
+
+Choice Unit_Lenin::MakeChoice()
+{
+    choice = (choice + 3 + (((last_enemy_choice - choice + 3) % 3 == 1) * 2 - 1))
+    return choice
+}
+
+void Unit_Lenin::SetResult(Choice enemy_choice)
+{
+    last_enemy_choice = enemy_choice;
+}
+
+void Unit_Lenin::StartBattle(int win_count);
+{
+    choice = 1;
+    last_enemy_choice = 0;
+    win_count_ = 0;
+}
--- Unit_Lenin.h
+++ Unit_Lenin.h
@@ -1,8 +1,28 @@
-#include  <BaseUnit.h>
+#include  <lib/BaseUnit.h>
 
-class Unit_Lenin{
+class Unit_Lenin : public BaseUnit{
     private:
-        
+        int choice = 1;
+        int last_enemy_choice = 0;
+        int win_count_ = 0;
 
+        const std::string name_ = "Lenin";
+
+        const std::string win_phrase_ = "Glory to the Soviet Union!";
+
+        const std::string lose_phrase_ = "This is a shame, comrades.";
+        
+    public:
+        Choice MakeChoice();
+
+        void SetResult(Choice enemy_choice);
+
+        void StartBattle(int win_count);
+
+        std::string GetName();
+
+        std::string GetWinPhrase();
+
+        std::string GetLosePhrase();
 };
 
