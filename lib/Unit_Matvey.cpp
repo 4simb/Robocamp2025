4a5f028fbe0814c8a7dc12190417baa9ea5ff855
MatveyF109 <matvey.fscool@gmail.com>
2025-07-30 14:06:01 +0400

    working version

--- lib/Unit_Matvey.cpp
+++ lib/Unit_Matvey.cpp
@@ -4,50 +4,68 @@
 Unit_Matvey::Unit_Matvey(){
     winsCnt = 0;
     lossCnt = 0;
+    drawsCnt = 0;
 }
 
-std::string Unit_Matvey::sayLoosePhrase(){
+std::string Unit_Matvey::GetLosePhrase(){
     return win_phrase_;
 }
-std::string Unit_Matvey::sayWinPhrase(){
+std::string Unit_Matvey::GetWinPhrase(){
     return lose_phrase_;
 }
 
-void Unit_Matvey::startBattle(){
+void Unit_Matvey::StartBattle(int n){
     winsCnt = 0;
     lossCnt = 0;
     oLog.clear();
     pLog.clear();
 }
 
-Choice Unit_Matvey::makeChoice(){
-    if(oLog.size() == 0){  //first play
-        currPlay = Choice::ROCK;
+Choice Unit_Matvey::MakeChoice(){
+    if (winsCnt + lossCnt + drawsCnt < 20){
+    
+        if(oLog.size() == 0){  //first play
+            currPlay = Choice::ROCK;
+            std::cout << "ROCK" << '\n';
+            return currPlay;
+        }
+
+        if(oLog[oLog.size() - 1] == 1){
+            currPlay = pLog[pLog.size() - 1];
+        } else {
+            Choice ch = pLog[pLog.size() - 1];
+            currPlay = ref[std::find(ref.begin(), ref.end(), ch) - ref.begin() + 1];
+        }
+        std::cout << lbl[std::find(ref.begin(), ref.end(), currPlay) - ref.begin()] << '\n';
         return currPlay;
     }
+    else {
+        if (winsCnt - lossCnt > 0){
 
-    if(oLog[oLog.size() - 1] == 1){
-        currPlay = pLog[pLog.size() - 1];
-    } else {
-        Choice ch = pLog[pLog.size() - 1];
-        currPlay = ref[std::find(ref.begin(), ref.end(), ch) - ref.begin() + 1];
-        // if (ch == Choice::ROCK){
-        //     currPlay = Choice::PAPER;
-        // }
-        // if (ch == Choice::PAPER){
-        //     currPlay = Choice::SCISSORS;
-        // }
-        // if (ch = Choice::SCISSORS){
-        //     currPlay = Choice::ROCK;
-        // }
+            if(oLog[oLog.size() - 1] == 1){
+                currPlay = pLog[pLog.size() - 1];
+            } else {
+                Choice ch = pLog[pLog.size() - 1];
+                currPlay = ref[std::find(ref.begin(), ref.end(), ch) - ref.begin() + 1];
+            }
+            std::cout << lbl[std::find(ref.begin(), ref.end(), currPlay) - ref.begin()] << '\n';
+            return currPlay;
+        }
+        else {
+            if(oLog[oLog.size() - 1] == 1){
+                Choice ch = pLog[pLog.size() - 1];
+                currPlay = ref[std::find(ref.begin(), ref.end(), ch) - ref.begin() + 1];
+            } else {
+                currPlay = pLog[pLog.size() - 1];
+            }
+
+        }
     }
-
-    return currPlay;
     
     
 }
 
-void Unit_Matvey::getResult(Choice ch){
+void Unit_Matvey::SetResult(Choice ch){
     pLog.push_back(ch);
 
     if (ch == currPlay){
@@ -59,5 +77,15 @@ void Unit_Matvey::getResult(Choice ch){
         oLog.push_back(-1);
         lossCnt++;
     }
+}
+
+void Unit_Matvey::sayOutcome(){
+    if (winsCnt > lossCnt){
+        std::cout << "WIN" << '\t';
+    } else {
+        std::cout << "LOSS" << '\t';
+    }
+
+    std::cout << winsCnt << ' ' << lossCnt;
 }
 
--- lib/Unit_Matvey.h
+++ lib/Unit_Matvey.h
@@ -8,25 +8,29 @@ class Unit_Matvey : public BaseUnit{
     public:
         Unit_Matvey();
 
-        std::string sayLoosePhrase();
-        std::string sayWinPhrase();
+        std::string GetLosePhrase();
+        std::string GetWinPhrase();
 
-        void startBattle();
+        void StartBattle(int n) override;
 
-        Choice makeChoice();
+        Choice MakeChoice() override;
 
-        void getResult(Choice ch);
+        void SetResult(Choice ch) override;
+
+        void sayOutcome();
 
     private:
         const std::string name_ = "Matvey";
         const std::string win_phrase_ = "Ill be back";
         const std::string lose_phrase_ = "OOOOOOOOOOOOOOOOOOOOOOH";
         std::vector<Choice> ref = {Choice::ROCK, Choice::PAPER, Choice::SCISSORS, Choice::ROCK};
+        std::vector<std::string> lbl = {"ROCK", "PAPER", "SCISSORS"};
 
         std::vector<int> oLog; //outcome log
         std::vector<Choice> pLog; //play log
         int winsCnt;
         int lossCnt;
+        int drawsCnt;
 
         Choice currPlay;
 };
